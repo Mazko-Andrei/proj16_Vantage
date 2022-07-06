bab10a09e6b8d980fae39378f630f76c8a4465d8
Andrei Mazko <andreimazko2022@gmail.com>
2022-07-06 21:06:23 +0300

    add three pages

--- js/script.js
+++ js/script.js
@@ -28,7 +28,7 @@ function toggleMobileNav() {
 let animeItems = document.querySelectorAll('.anim-items');
 
 if (animeItems.length > 0) {
-    window.addEventListener('scroll', animOnScroll); 
+    window.addEventListener('scroll', animOnScroll);
     function animOnScroll(params) {
         for (let i = 0; i < animeItems.length; i++) {
             const animItem = animeItems[i];
@@ -38,14 +38,14 @@ if (animeItems.length > 0) {
 
             let animeItemPoint = window.innerHeight - animeItemHeight / animStart;
 
-            if(animeItemHeight > window.innerHeight){
+            if (animeItemHeight > window.innerHeight) {
                 animeItemPoint = window.innerHeight - window.innerHeight / animStart;
             }
 
-            if((scrollY > animeItemOffset - animeItemPoint) && scrollY < (animeItemOffset + animeItemHeight)){
+            if ((scrollY > animeItemOffset - animeItemPoint) && scrollY < (animeItemOffset + animeItemHeight)) {
                 animItem.classList.add('active');
             } else {
-                if(!animItem.classList.contains('anim-no-hide')){
+                if (!animItem.classList.contains('anim-no-hide')) {
                     animItem.classList.remove('active');
                 }
             }
@@ -60,5 +60,102 @@ if (animeItems.length > 0) {
     setTimeout(() => {
         animOnScroll();
     }, 300);
-    
 }
+
+// Числовая анимация
+
+window.addEventListener("load", windowload);
+
+function windowload() {
+    function digitsCountersInit(digitsCountersItems) {
+        let digitsCounters = digitsCountersItems ? digitsCountersItems : document.querySelectorAll("[data-digits-counter]");
+        if (digitsCounters) {
+            digitsCounters.forEach(digitsCounter => {
+                digitsCountersAnimate(digitsCounter);
+            });
+        }
+    }
+
+
+    function digitsCountersAnimate(digitsCounter) {
+        let startTimestamp = null;
+        const duration = parseInt(digitsCounter.dataset.digitsCounter) ? parseInt(digitsCounter.dataset.digitsCounter) : 1000;
+        const startValue = parseInt(digitsCounter.innerHTML);
+        const startPosition = 0;
+        const step = (timestamp) => {
+            if (!startTimestamp) startTimestamp = timestamp;
+            const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+            digitsCounter.innerHTML = Math.floor(progress * (startPosition + startValue));
+            if (progress < 1) {
+                window.requestAnimationFrame(step);
+            }
+        };
+        window.requestAnimationFrame(step);
+    }
+    // digitsCountersInit();
+    let options = {
+        threshold: 0.3
+    }
+
+    let observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                const targetElement = entry.target;
+                const digitsCountersItems = targetElement.querySelectorAll("[data-digits-counter]");
+                if (digitsCountersItems.length) {
+                    digitsCountersInit(digitsCountersItems);
+                }
+                // observer.unobserve(targetElement); // что-бы анимация повторно не запускалась
+            }
+        });
+    }, options);
+
+    let sections = document.querySelectorAll('.page_section');
+    if (sections.length) {
+        sections.forEach(section => {
+            observer.observe(section);
+        });
+    }
+}
+
+// Swiper 
+
+var swiper = new Swiper(".numberSwiper", {
+    slidesPerView: 2,
+    pagination: {
+        el: ".swiper-pagination",
+        dynamicBullets: true,
+    },
+    // navigation: {
+    //     nextEl: ".swiper-button-next",
+    //     prevEl: ".swiper-button-prev",
+    // },
+    breakpoints: {
+        425: {
+            slidesPerView: 3,
+        },
+        920: {
+            slidesPerView: 5,
+
+        }
+    }
+});
+
+var swiper = new Swiper(".imgSwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    breakpoints: {
+        600: {
+            slidesPerView: 2,
+        },
+        920: {
+            slidesPerView: 3,
+
+        }
+    }
+  });
+
